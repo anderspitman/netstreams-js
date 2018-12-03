757ebbfc974d1d0b4f2c9d964ad6a2ea9b4ac863
Anders Pitman <tapitman11@gmail.com>
2018-12-03 15:18:24 -0700

    Sort of working for fibridge

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const { FileChunker } = require('file-chunk-reader')
+const ab2str = require('arraybuffer-to-string')
+const str2ab = require('string-to-arraybuffer')
+
 const MESSAGE_TYPE_CREATE_STREAM = 0
 const MESSAGE_TYPE_STREAM_DATA = 1
+const MESSAGE_TYPE_STREAM_END = 2
+const MESSAGE_TYPE_TERMINATE_STREAM = 3
 
 
 class Peer {
@@ -20,6 +26,33 @@ class Peer {
     this._connections[id] = connection
     return connection
   }
+
+  createWebsocketConnection(ws) {
+
+    return new Promise(function(resolve, reject) {
+
+      const conn = new Connection
+
+      ws.binaryType = 'arraybuffer'
+
+      ws.onopen = (event) => {
+
+        conn.setSendHandler((message) => {
+          ws.send(message)
+        })
+
+        ws.onmessage = (rawMessage) => {
+          conn.onMessage(rawMessage)
+        }
+
+        resolve(conn);
+      }
+
+      ws.onerror = (err) => {
+        reject(err);
+      }
+    });
+  }
 }
 
 
@@ -36,16 +69,19 @@ class Connection {
 
     switch (message.type) {
       case MESSAGE_TYPE_CREATE_STREAM: {
-        console.log("Create new stream: " + message.streamId)
+        console.log("Create stream: " + message.streamId)
 
         const stream = this._makeStream(message.streamId)
         this._remoteStreams[message.streamId] = stream
-        this._onStream(stream)
+
+        const metadata = JSON.parse(ab2str(message.data))
+
+        this._onStream(stream, metadata)
 
         break;
       }
       case MESSAGE_TYPE_STREAM_DATA: {
-        console.log("Stream data for stream: " + message.streamId)
+        //console.log("Stream data for stream: " + message.streamId)
 
         const stream = this._remoteStreams[message.streamId]
         if (stream) {
@@ -55,6 +91,12 @@ class Connection {
           console.error("Invalid stream id: " + message.streamId)
         }
 
+        break;
+      }
+      case MESSAGE_TYPE_STREAM_END: {
+        console.log("Stream ended: " + message.streamId)
+        const stream = this._remoteStreams[message.streamId]
+        stream._onEnd()
         break;
       }
       default: {
@@ -76,12 +118,27 @@ class Connection {
     const id = this.nextStreamId()
     const stream = this._makeStream(id)
     this._localStreams[id] = stream
-    this._signalCreateStream(id)
+    this._signalCreateStream(id, metadata)
+    stream.onEnd(() => {
+      const message = new Uint8Array(2)
+      message[0] = MESSAGE_TYPE_STREAM_END
+      message[1] = id 
+      this._send(message)
+    })
     return stream
   }
 
   _makeStream(id) {
-    const stream = new Stream(id, this._streamSend.bind(this))
+    const sendFunc = this._streamSend.bind(this)
+    const writeFunc = (data) => {
+      sendFunc(id, data)
+    }
+
+    const terminateFunc = () => {
+      this._streamTerminate(id)
+    }
+
+    const stream = new Stream(id, writeFunc, terminateFunc)
     return stream
   }
 
@@ -91,17 +148,33 @@ class Connection {
     return next
   }
 
-  _signalCreateStream(streamId) {
+  _signalCreateStream(streamId, metadata) {
+
+    const mdString = JSON.stringify(metadata)
+    const mdArray = new Uint8Array(str2ab(mdString))
+
     // TODO: allow stream ids to go higher than 256, or at least reuse them
-    const message = new Uint8Array(2)
+    const message = new Uint8Array(2 + mdArray.byteLength)
     message[0] = MESSAGE_TYPE_CREATE_STREAM
+    message[1] = streamId
+
+    for (let i = 0; i < mdArray.byteLength; i++) {
+      message[i+2] = mdArray[i]
+    }
+    
+    this._send(message)
+  }
+
+  _signalTerminateStream(streamId) {
+    const message = new Uint8Array(2)
+    message[0] = MESSAGE_TYPE_TERMINATE_STREAM
     message[1] = streamId
 
     this._send(message)
   }
 
   _streamSend(streamId, data) {
-    const message = new Uint8Array(2 + data.length)
+    const message = new Uint8Array(2 + data.byteLength)
     message[0] = MESSAGE_TYPE_STREAM_DATA
     message[1] = streamId 
 
@@ -109,6 +182,20 @@ class Connection {
       message[i+2] = data[i]
     }
     this._send(message)
+  }
+
+  _streamTerminate(streamId) {
+
+    if (this._isLocalStream(streamId)) {
+      console.log("terminate local stream: " + streamId)
+      // inform the remote peer and delete local reference
+      this._signalTerminateStream(streamId)
+      //delete this._localStreams[streamId]
+    }
+    else {
+      console.log("terminate remote stream: " + streamId)
+      //delete this._remoteStreams[streamId]
+    }
   }
 
   _parseMessage(rawMessage) {
@@ -119,26 +206,62 @@ class Connection {
     message.data = byteMessage.slice(2)
     return message
   }
+
+  _isLocalStream(streamId) {
+    return this._localStreams[streamId] !== undefined
+  }
 }
 
 
 class Stream {
-  constructor(id, sendFunc) {
+  constructor(id, write, terminate, chunkSize) {
     this.id = id
-    this._send = sendFunc
+    this._write = write 
+    this._terminate = terminate
+    this._chunkSize = chunkSize ? chunkSize : 1024
+    this._onData = () => {}
+    this._onEnd = () => {}
   }
 
   write(data) {
-    this._send(this.id, data)
+    this._write(new Uint8Array(data))
+  }
+
+  writeFile(file) {
+
+    const chunker = new FileChunker(file, {
+      binary: true,
+      chunkSize: 1 * 1024 * 1024,
+    })
+
+    chunker.onChunk((chunk) => {
+      console.log("send chunk")
+      this.write(chunk)
+    });
+
+    chunker.onEnd(() => {
+      this._onEnd()
+    });
+
+    chunker.read();
+  }
+
+  terminate() {
+    this._terminate()
   }
 
   onData(callback) {
     this._onData = callback
   }
 
+  onEnd(callback) {
+    this._onEnd = callback
+  }
+
   onReceive(data) {
     this._onData(data)
   }
+
 }
 
 module.exports = {
