a0aac6a556bb9cd6602d2b204165525b2ad8f1d2
Anders Pitman <tapitman11@gmail.com>
2018-12-14 14:33:19 -0700

    Implement basic Stream base classes

---
+++ index.js
@@ -0,0 +1,90 @@
+class Stream {
+  onError(callback) {
+    this._errorCallback = callback
+  }
+}
+
+class ReadStream extends Stream {
+  constructor() {
+    super()
+    //this._dataCallback = () => {}
+    //this._endCallback = () => {}
+    this._demand = 0
+  }
+
+  request(numBytes) {
+    this._demand += numBytes
+    this._demandChanged()
+  }
+
+  pipe(writeStream) {
+    this._dataCallback = (data) => {
+      writeStream.write(data)
+    }
+
+    this.onEnd(() => {
+      writeStream.end()
+    })
+
+    writeStream.onRequest((numBytes) => {
+      this.request(numBytes)
+    })
+  }
+
+  onData(callback) {
+    this._dataCallback = callback
+  }
+
+  onEnd(callback) {
+    this._endCallback = callback
+  }
+
+  _demandChanged() {
+    throw "_demandChanged must be implemented"
+  }
+}
+
+
+class WriteStream extends Stream {
+  constructor(options) {
+    super()
+
+    const opts = options ? options : {}
+
+    this._bufferSize = opts.bufferSize ? opts.bufferSize : 1024*1024
+    this._demand = this._bufferSize
+
+    this._endCallback = () => {}
+  }
+
+  write(data) {
+    if (data.byteLength > this._demand) {
+      this._errorCallback("Attempt to write more than requested amount of data")
+    }
+
+    this._demand += data.byteLength
+    this._write(data)
+  }
+
+  end() {
+    this._endCallback()
+  }
+
+  onRequest(callback) {
+    this._onRequestCallback = callback
+    callback(this._bufferSize)
+  }
+
+  onEnd(callback) {
+    this._endCallback = callback
+  }
+
+  _write() {
+    throw "_write must be implemented"
+  }
+}
+
+module.exports = {
+  ReadStream,
+  WriteStream,
+}
