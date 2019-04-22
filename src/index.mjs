91cb1fbb403fb8896f4908e2e05a41524f88b97d
Anders Pitman <tapitman11@gmail.com>
2019-04-22 11:36:29 -0600

    Remove unused code

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,2 @@
-import { MuxReceiver } from './mux_receiver.mjs';
-import { MuxSender } from './mux_sender.mjs';
-import { WebSocketInitiator } from './transport.mjs';
-
-
-const MESSAGE_TYPE_CREATE_RECEIVER = 0
-const MESSAGE_TYPE_STREAM_DATA = 1
-const MESSAGE_TYPE_STREAM_END = 2
-const MESSAGE_TYPE_TERMINATE_SENDER = 3
-const MESSAGE_TYPE_STREAM_REQUEST_DATA = 4
-const MESSAGE_TYPE_CONTROL_MESSAGE = 5
-
-
+export { WebSocketInitiator } from './transport.mjs';
 export { Multiplexer } from './multiplexer.mjs';
